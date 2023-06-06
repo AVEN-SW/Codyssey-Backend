1019cad0b28803391aba48aec280d565441d5985
SNOW <kancth03@gmail.com>
2023-06-07 04:50:44 +0900

    Fix: OauthContoller service 기능 분리

--- src/main/java/com/cement/CodysseyBackend/domain/oauth/github/controller/GithubController.java
+++ src/main/java/com/cement/CodysseyBackend/domain/oauth/github/controller/GithubController.java
@@ -1,6 +1,8 @@
 package com.cement.CodysseyBackend.domain.oauth.github.controller;
 
+import com.cement.CodysseyBackend.domain.oauth.github.service.GithubService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,66 +13,17 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RestController
 public class GithubController {
 
+    private GithubService githubService;
+
     @GetMapping("/auth/github/callback")
     public String getCode(@RequestParam String code, RedirectAttributes redirectAttributes) throws IOException {
+        String responseData = githubService.getCode(code);
 
-        URL url = new URL("https://github.com/login/oauth/access_token");
-
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
-
-        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()))) {
-            bw.write("client_id=ae435d706af35e3a7041&client_secret=41116a6f2e7c65801e2f1394f527ce8bab78cd7d&code=" + code);
-            bw.flush();
-        }
-
-        int responseCode = conn.getResponseCode();
-
-        String responseData = getResponse(conn, responseCode);
-
-        conn.disconnect();
-
-        String githubData = access(responseData, redirectAttributes);
+        String githubData = githubService.access(responseData, redirectAttributes);
         return githubData;
-    }
-
-    public String access(String response, RedirectAttributes redirectAttributes) throws IOException {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, String> map = objectMapper.readValue(response, Map.class);
-        String access_token = map.get("access_token");
-
-        URL url = new URL("https://api.github.com/user");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
-        conn.setRequestProperty("Authorization", "token " + access_token);
-
-        int responseCode = conn.getResponseCode();
-
-        String result = getResponse(conn, responseCode);
-        conn.disconnect();
-        return result;
-    }
-
-    private String getResponse(HttpURLConnection conn, int responseCode) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        if (responseCode == 200) {
-            try (InputStream is = conn.getInputStream();
-                 BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-                for (String line = br.readLine(); line != null; line = br.readLine()) {
-                    sb.append(line);
-                }
-            }
-        }
-        return sb.toString();
     }
 }
--- src/main/java/com/cement/CodysseyBackend/domain/oauth/github/service/GithubService.java
+++ src/main/java/com/cement/CodysseyBackend/domain/oauth/github/service/GithubService.java
@@ -1,7 +1,72 @@
 package com.cement.CodysseyBackend.domain.oauth.github.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Map;
 
 @Service
 public class GithubService {
+
+    public String getCode(String code) throws IOException {
+
+        URL url = new URL("https://github.com/login/oauth/access_token");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
+
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()))) {
+            bw.write("client_id=ae435d706af35e3a7041&client_secret=41116a6f2e7c65801e2f1394f527ce8bab78cd7d&code=" + code);
+            bw.flush();
+        }
+
+        int responseCode = conn.getResponseCode();
+        String responseData = getResponse(conn, responseCode);
+        conn.disconnect();
+
+        return responseData;
+    }
+
+    public String access(String response, RedirectAttributes redirectAttributes) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> map = objectMapper.readValue(response, Map.class);
+        String access_token = map.get("access_token");
+
+        URL url = new URL("https://api.github.com/user");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
+        conn.setRequestProperty("Authorization", "token " + access_token);
+
+        int responseCode = conn.getResponseCode();
+
+        String result = getResponse(conn, responseCode);
+        conn.disconnect();
+        return result;
+    }
+
+    private String getResponse(HttpURLConnection conn, int responseCode) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        if (responseCode == 200) {
+            try (InputStream is = conn.getInputStream();
+                 BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+                for (String line = br.readLine(); line != null; line = br.readLine()) {
+                    sb.append(line);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
 }
