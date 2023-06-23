bd036b014284072f065fa24bef979174b5517e3d
Hyowon Kang <hyowon3434@naver.com>
2023-06-23 21:34:22 +0900

    Feat: 패키지 생성
    도메인 패키지 생성

---
+++ src/main/java/com/cement/CodysseyBackend/domain/board/community/communicty.java
@@ -0,0 +1,4 @@
+package com.cement.CodysseyBackend.domain.board.community;
+
+public class communicty {
+}
--- src/main/java/com/cement/CodysseyBackend/domain/member/practice.java
+++
@@ -1,5 +0,0 @@
-package com.cement.CodysseyBackend.domain.member;
-
-public class practice {
-    
-}
