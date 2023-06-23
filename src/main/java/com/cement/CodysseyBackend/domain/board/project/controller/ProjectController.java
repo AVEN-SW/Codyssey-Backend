94c5774ff211f9a250a0ba7e69dc9c67b7474a2f
SNOW <kancth03@gmail.com>
2023-06-23 23:51:09 +0900

    Docs: 사용하지 않는 import 제거

--- src/main/java/com/cement/CodysseyBackend/domain/board/project/controller/ProjectController.java
+++ src/main/java/com/cement/CodysseyBackend/domain/board/project/controller/ProjectController.java
@@ -6,8 +6,6 @@ import com.cement.CodysseyBackend.domain.board.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
