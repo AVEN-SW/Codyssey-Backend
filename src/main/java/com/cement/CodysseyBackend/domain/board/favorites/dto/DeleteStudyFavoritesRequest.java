e8dd2b17d7067e3a7f6664ae415f33fbf81d7690
SNOW <kancth03@gmail.com>
2023-07-08 23:55:13 +0900

    Feat: Study 게시판 즐겨찾기 삭제 기능 구현

---
+++ src/main/java/com/cement/CodysseyBackend/domain/board/favorites/dto/DeleteStudyFavoritesRequest.java
@@ -0,0 +1,10 @@
+package com.cement.CodysseyBackend.domain.board.favorites.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteStudyFavoritesRequest {
+    private Long studyBookmarkId;
+}
