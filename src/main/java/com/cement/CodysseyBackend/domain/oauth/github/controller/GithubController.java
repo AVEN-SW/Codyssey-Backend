95420459a483b9779f9a13fcef8df7c2ebc1e686
SNOW <kancth03@gmail.com>
2023-07-10 19:55:41 +0900

    Fix: 로그인 플로우 변경

--- src/main/java/com/cement/CodysseyBackend/domain/oauth/github/controller/GithubController.java
+++ src/main/java/com/cement/CodysseyBackend/domain/oauth/github/controller/GithubController.java
@@ -5,9 +5,11 @@ import com.cement.CodysseyBackend.domain.oauth.github.dto.AccessTokenResponse;
 import com.cement.CodysseyBackend.domain.oauth.github.dto.GithubLoginResponse;
 import com.cement.CodysseyBackend.domain.oauth.github.service.GithubService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,10 +19,14 @@ public class GithubController {
 
     private final GithubService githubService;
 
-    @GetMapping("/auth/github/callback")
-    public ResponseEntity<AccessTokenResponse> getCode(@RequestParam String code, RedirectAttributes redirectAttributes){
+    @GetMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public AccessTokenResponse getCode(@RequestParam String code) {
+        // code로 AccessToken 가져오기
         String responseData = githubService.getCode(code);
+        // response에서 AccessToken만 가져오기
         String access_token = githubService.getAccessToken(responseData);
+        // AccessToken으로 정보 가져오기
         GithubLoginResponse githubData = githubService.access(access_token);
 
         Access response = Access.builder()
@@ -36,12 +42,10 @@ public class GithubController {
                 .access_token(access_token)
                 .isCreated(githubData.isCreated())
                 .build();
-
         if (isCreated) {
             // Member가 있을 경우 Member Table에 Access Token 등록
             githubService.saveMemberAccessToken(response);
         }
-        return ResponseEntity.ok()
-                .body(accessTokenResponse);
+        return accessTokenResponse;
     }
 }
